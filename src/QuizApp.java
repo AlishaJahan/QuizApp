e3d09ea7c87d688ffb7384dc08f28a54f6ca3aee
AlishaJahan <jahanalisha17@gmail.com>
2025-05-04 22:17:27 +0530

    question changed

--- src/QuizApp.java
+++ src/QuizApp.java
@@ -8,7 +8,7 @@ public class QuizApp {
         String[] questions = {
             "1. What is the capital of France?\nA. Rome\nB. Paris\nC. Berlin\nD. Madrid",
             "2. Which planet is known as the Red Planet?\nA. Venus\nB. Mars\nC. Jupiter\nD. Saturn",
-            "3. What is 5 + 7?\nA. 10\nB. 11\nC. 12\nD. 13"
+            "3. who is the chemical symbol of gold?\nA. Gd\nB. Af\nC. Ag\nD. Rq"
         };
 
         char[] answers = {'B', 'B', 'C'};
